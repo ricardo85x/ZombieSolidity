854ef721ba6a059d703b1d17f7d57bb08ce904d1
Ricardo <ricardoinliner@yahoo.com>
2021-03-18 18:21:05 -0300

    Zombi is attacking. Run

--- ZombieBattle.sol
+++ ZombieBattle.sol
@@ -12,8 +12,23 @@ contract ZombieBattle is ZombieHelper {
         return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
     } 
 
-    function attack(uint _zombieId, uint _targetId) external {
-      
+    function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+
+        uint rand = randMod(100);
+
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount++;
+            enemyZombie.lossCount++;
+            enemyZombie.level++;
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+        }
+        _triggerCooldown(myZombie);
+    
     }
     
 }
